ffca4ac5079d5f777437487f203bbd79bafe86a1
philipe <philipegama97@gmail.com>
2020-07-28 11:46:27 -0400

    novos contéudos e exercícios

---
+++ Curso Intensivo de Python Uma Eric Mat/exercicio/6.1.py
@@ -0,0 +1,9 @@
+pessoa_0 = {'first_name' : 'john',
+			 'last_name' : 'doe',
+			 'age' : 25,
+			 'city': 'atlanta', 
+			  }
+
+print('Name: '+pessoa_0['first_name'].title()+' '+pessoa_0['last_name'].title())
+print('Age: '+ str(pessoa_0['age']))
+print('City: '+pessoa_0['city'].title())
